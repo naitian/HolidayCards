6f4be13aef78fab4e9dcfb9e529e0f70ca8b6764
Naitian Zhou <naitian@naitian.org>
2020-02-18 18:39:04 -0500

    Card 2019

--- template/js/whereintheworld.js
+++ template/js/whereintheworld.js
@@ -1,55 +1,3 @@
-function drawMap() {
-  const width = 800,
-    height = 500;
-  const projection = d3
-    .geoOrthographic()
-    .scale(200)
-    .translate([width / 2, height / 2])
-    .rotate([83, -42, 0])
-    .clipAngle(90)
-    .precision(0.3);
-  const path = d3.geoPath().projection(projection);
-  const svg = d3.select("svg");
-  const g = svg.append("g");
-  g.append("path")
-    .datum({ type: "Sphere" })
-    .attr("class", "sphere")
-    .style("stroke", "black")
-    .attr("d", path)
-    .attr("fill", "#fefefe");
-
-  Promise.all([
-    d3.json(
-      "https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-110m.json"
-    )
-  ]).then(([worldData]) => {
-    svg
-      .selectAll(".segment")
-      .data(topojson.feature(worldData, worldData.objects.countries).features)
-      .enter()
-      .append("path")
-      .attr("class", "segment")
-      .attr("d", path)
-      // .style("stroke", "#888")
-      // .style("stroke-width", "1px")
-      .style("fill", (d, i) => "#e5e5e5")
-      .style("opacity", ".6");
-    // locations = locationData;
-    svg
-      .selectAll(".marker")
-      .data([{ city: "Ann Arbor", lat: 42.2808, lng: -83.743 }])
-      .enter()
-      .append("circle")
-      .each(function(d) {
-        [x, y] = projection([d.lng, d.lat]);
-        d3.select(this)
-          .attr("r", 4)
-          .attr("transform", d => `translate(${x}, ${y})`)
-          .attr("fill", "black");
-      });
-  });
-}
-
 // https://observablehq.com/@d3/world-tour
 class Versor {
   static fromAngles([l, p, g]) {
@@ -124,7 +72,6 @@ class Versor {
 }
 
 console.log("Where in the world...");
-// drawMap();
 
 class Mappy {
   constructor(canvas, world, tilt = 20) {
@@ -136,13 +83,19 @@ class Mappy {
     this.land = topojson.feature(world, world.objects.land);
     this.sphere = { type: "Sphere" };
 
-    this.lng = 0;
-    this.lat = 0;
+    this.lng = -83;
+    this.lat = 42;
+    this.zoomLevel = d3.zoomIdentity;
+    this.initialScale = 400;
+    this.placeName = null;
+
+    this.mapColor = "#C37570";
+    this.textColor = "#fbeed6";
 
     this.context = canvas.getContext("2d");
     this.projection = d3
       .geoOrthographic()
-      .scale(400)
+      .scale(this.initialScale)
       .translate([this.width / 2, this.height / 2])
       .clipAngle(90)
       .precision(0.3);
@@ -151,18 +104,20 @@ class Mappy {
     this.render = this.render.bind(this);
     this.setCoords = this.setCoords.bind(this);
 
-    this.setCoords(this.lng, this.lat);
+    this.setCoords(this.lng, this.lat, "");
   }
 
   getCanvas() {
     return this.context.canvas;
   }
 
-  render(placeName) {
+  render() {
     this.context.clearRect(0, 0, this.width, this.height);
     this.context.beginPath(),
       this.path(this.land),
-      (this.context.fillStyle = "#ccc"),
+      // (this.context.strokeStyle = this.mapColor),
+      // (this.context.lineWidth = 2),
+      (this.context.fillStyle = this.mapColor),
       this.context.fill();
     // this.context.beginPath(),
     //   this.path(borders),
@@ -171,28 +126,47 @@ class Mappy {
     //   this.context.stroke();
     this.context.beginPath(),
       this.path(this.sphere),
-      (this.context.strokeStyle = "#000"),
-      (this.context.lineWidth = 0.5),
+      (this.context.strokeStyle = this.mapColor),
+      (this.context.lineWidth = 2),
       this.context.stroke();
 
-    if (placeName) {
+    if (this.placeName) {
       let [x, y] = this.projection([this.lng, this.lat]);
-      console.log(x, y);
-      this.context.fillStyle = "red";
-      this.context.beginPath(),
-        this.context.arc(x, y, 4, 0, 2 * Math.PI),
-        this.context.fill();
-      this.context.beginPath(),
-        this.context.font = "16px Calistoga";
-        this.context.fillText(placeName, x + 10, y - 10);
+      if (x && y) {
+        this.context.fillStyle = this.textColor;
+        this.context.beginPath(),
+          this.context.arc(x, y, 4, 0, 2 * Math.PI),
+          this.context.fill();
+        this.context.beginPath(), (this.context.font = "24px Calistoga");
+        this.context.fillText(this.placeName, x + 10, y - 10);
+      }
     }
     return this.context.canvas;
   }
 
-  async setCoords(lng, lat, placeName) {
+  async zoomMap(zoom) {
+    const iz = d3.interpolateArray(
+      [this.zoomLevel.x, this.zoomLevel.y, this.zoomLevel.k],
+      [zoom.x, zoom.y, zoom.k]
+    );
+    this.zoomLevel = zoom;
+    await d3
+      .transition()
+      .duration(400)
+      .tween("render", () => t => {
+        let view = iz(t);
+        this.projection
+          .scale(this.initialScale * view[2])
+          .translate([this.width / 2 - view[0], this.height / 2 - view[1]]);
+        requestAnimationFrame(this.render);
+      }).end();
+  }
+
+  async setCoords(lng, lat, placeName = null) {
     let r1 = [-this.lng, this.tilt - this.lat, 0];
     this.lng = lng;
     this.lat = lat;
+    this.placeName = placeName;
     let r2 = [-this.lng, this.tilt - this.lat, 0];
     const iv = Versor.interpolateAngles(r1, r2);
     await d3
@@ -200,9 +174,8 @@ class Mappy {
       .duration(1250)
       .tween("render", () => t => {
         this.projection.rotate(iv(t));
-        this.render(placeName);
+        requestAnimationFrame(this.render);
       })
-      .end();
   }
 }
 
@@ -210,8 +183,21 @@ async function init() {
   const world = await d3.json(
     "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json"
   );
+
   const canvas = document.querySelector(".map canvas");
   const mappy = new Mappy(canvas, world);
+
+  const ipaddr = await d3.json("https://api.ipify.org/?format=json");
+  // sorry for using this API key... 
+  const loc = await d3.json(
+    `https://api.ipstack.com/${
+      ipaddr.ip
+    }?access_key=fd4d87f605681c0959c16d9164ab6a4a&format=1`
+  );
+  let placeName = loc.city,
+    lat = loc.latitude,
+    lng = loc.longitude;
+  if (placeName) mappy.setCoords(lng, lat, placeName + " (you are here)");
 
   const scroller = scrollama();
   scroller
@@ -219,15 +205,22 @@ async function init() {
       step: ".step",
       offset: 0.7
     })
-    .onStepEnter(response => {
+    .onStepEnter(async response => {
       // { element, index, direction }
       place = window.places[response.index];
-      if (place.lng && place.lat) {
+      if (response.index === 0 && response.direction === "down") {
+        await mappy.zoomMap(d3.zoomIdentity.translate(0, -300).scale(3));
+      }
+      if (place && place.lng && place.lat) {
         mappy.setCoords(place.lng, place.lat, place.location);
       }
     })
-    .onStepExit(response => {
+    .onStepExit(async response => {
       // { element, index, direction }
+      if (response.index === 0 && response.direction === "up") {
+        await mappy.zoomMap(d3.zoomIdentity);
+        mappy.setCoords(lng, lat, placeName);
+      }
     });
 
   window.addEventListener("resize", scroller.resize);
